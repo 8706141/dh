bea415239f7eb0e468cece8de3609a1de86852d5
8706141 <49219563+8706141@users.noreply.github.com>
2023-09-16 18:31:46 +0800

    Update bd.js

--- include/bd.js
+++ include/bd.js
@@ -8,7 +8,41 @@ var bd = {
 		{"name": "小红书","action": "https://www.xiaohongshu.com/search_result?keyword=","website": "https://www.xiaohongshu.com/"},
 		{"name": "shutterstock","action": "https://www.shutterstock.com/zh/search/","website": "https://www.shutterstock.com/"},
 		{"name": "昵图","action": "https://soso.nipic.com/?q=","website": "https://nipic.com/"},
+		{"name": "图行天下","action": "https://www.photophoto.cn/all/?q.html","website": "https://www.photophoto.cn/"},
 		{"name": "翻译","action": "https://fanyi.baidu.com/#zh/en/","website": "https://fanyi.baidu.com/"}
-	]
+	],
+	"link": [
+		{
+			"pagination": "常用",
+			"contain": 
+			[
+				{"name": "后台","link": "https://github.com/8706141/dh/blob/main/include/bd.js","icon": "","span": "后"},
+				{"name": "导航","link": "https://8706141.github.io/nav/","icon": "","span": "导"},
+				{"name": "贴吧","link": "https://tieba.baidu.com/index.html","icon": "","span": "du"},
+				{"name": "淘个图","link": "https://www.taogetu.com/materialDownload","icon": "","span": "淘"}
 
+			]
+		},
+		{
+			"pagination": "网站",
+			"contain": 
+			[
+				{"name": "少数派","link": "https://sspai.com/","icon": "pi","span": ""},
+				{"name": "github","link": "https://github.com/","icon": "github","span": ""},
+				{"name": "菜鸟","link": "https://www.runoob.com/","icon": "language-html5","span": ""},
+				{"name": "w3school","link": "https://www.w3school.com.cn/","icon": "","span": "W3"},
+				{"name": "MDI","link": "https://materialdesignicons.com/","icon": "vector-square","span": ""}
+			]
+		},
+		{
+			"pagination": "工具",
+			"contain": 
+			[
+				{"name": "爱奇艺","link": "https://www.iqiyi.com/"，"icon": "","span": "QIY"},
+				{"name": "腾讯视频","link": "https://v.qq.com/"，"icon": "google-play","span": ""},
+				{"name": "优酷","link": "https://www.youku.com/"，"icon": "play-box","span": ""},
+				{"name": "CCTV","link": "https://tv.cctv.com/live/cctv1/?"，"icon": "cctv","span": ""}
+			]
+		}
+	]
 };
