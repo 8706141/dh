13997b0da3e13cef23b016dfa6e998411cf8e802
8706141 <49219563+8706141@users.noreply.github.com>
2023-09-16 14:59:43 +0800

    Update bd.js

--- include/bd.js
+++ include/bd.js
@@ -17,7 +17,6 @@ var bd = {
 			"contain": 
 			[
 				{"name": "后台","link": "https://github.com/8706141/dh/blob/main/include/bd.js","icon": "","span": "后"},
-				{"name": "微博","link": "https://weibo.com/","icon": "sina-weibo","span": ""},
 				{"name": "导航","link": "https://8706141.github.io/nav/","icon": "","span": "导"},
 				{"name": "贴吧","link": "https://tieba.baidu.com/index.html","icon": "","span": "du"},
 				{"name": "淘个图","link": "https://www.taogetu.com/materialDownload","icon": "","span": "淘"}
